e3165d686e8a31c2abce55d7dfd8ebf8fd0c4a9c
Isaiah.S <143666137+IsaiahSantiago@users.noreply.github.com>
2023-11-01 20:30:42 -0400

    Add files via upload

---
+++ Introductions.py
@@ -0,0 +1,20 @@
+# Write a function that takes 2 pieces of input (2 people), and introduce one to another
+# Prompt the user for two names
+# Call your function, which should then print an introduction: introducing one person to the other
+
+def introduce(person1, person2):
+    print(person1, ", allow me to introduce you to", person2, "!")
+
+p1 = input("Name a person: ")
+p2 = input("Name another person: ")
+
+introduce(p1, p2)
+#P1 and P2 are the arguments
+#person1 and person2 are parameters
+#
+
+
+# intro = input("hello there...")
+# intro = input("hello there...")
+# print("Why hello there " + intro)
+# print("Why hello there " + intro)
